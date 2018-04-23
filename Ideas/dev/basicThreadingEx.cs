3dc7974d2a9f9b842ecbf191945f22f6ff3fff3b
adrianB3 <balanescuadrian71@gmail.com>
2018-04-23 21:15:16 +0300

    added some threading examples

---
+++ Ideas/dev/basicThreadingEx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading; // added threading namespace
+
+namespace threadingBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadOne = new Thread(Work1); // create threads
+            Thread threadTwo = new Thread(Work2);
+
+            threadOne.Start(); // start threads
+            threadTwo.Start();
+
+        }
+
+        static void Work1()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work 1 is called: " + i.ToString());
+            }
+        }
+
+        static void Work2()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work 2 is called: " + i.ToString());
+            }
+        }
+    }
+}
---
+++ Ideas/dev/sleep_method.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading; // added threading namespace
+using System.Diagnostics; // for Stopwatch
+
+namespace threadingBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch st = new Stopwatch(); // executes on main thread
+            st.Start(); // starts counting
+
+            Thread justAThread = new Thread(ProcessSleep);
+
+            justAThread.Start();
+            justAThread.Join(); // main thread now waits for justAThread to finish
+
+            st.Stop();
+
+            TimeSpan ts = st.Elapsed; // getting the elapsed time as a timespan
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds); // formatting the timespan
+
+            Console.WriteLine("Total time : " + elapsedTime);
+            Console.WriteLine("Work completed!");
+        }
+
+        static void ProcessSleep()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Thread one working...");
+                Thread.Sleep(4000);  // justAThread will sleep for 4000ms
+            }
+        }
+    }
+}
+
+/* OUTPUT: 
+Thread one working...
+Thread one working...
+Total time : 00:00:08
+Work completed!
+Press any key to continue . . .
+*/
+
