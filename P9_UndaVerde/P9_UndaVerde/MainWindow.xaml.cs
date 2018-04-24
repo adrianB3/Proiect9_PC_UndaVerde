5010f6e7151234dcaa422895e06d3ee26ac09d89
adrianB3 <balanescuadrian71@gmail.com>
2018-04-24 19:37:51 +0300

    Added material design interface and a test route on the map

--- P9_UndaVerde/P9_UndaVerde/MainWindow.xaml.cs
+++ P9_UndaVerde/P9_UndaVerde/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,20 +30,42 @@ namespace P9_UndaVerde
         }
         private void mapView_Loaded(object sender, RoutedEventArgs e)
         {
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
             
             mapView.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
             mapView.MinZoom = 2;
-            mapView.MaxZoom = 17;
+            mapView.MaxZoom = 21;
            
-            mapView.Zoom = 2;
-            
-            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            mapView.Zoom = 16;
+            mapView.Position = new PointLatLng(45.7366683, 21.2277931);
+            mapView.ShowCenter = false;
+            mapView.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
             
             mapView.CanDragMap = true;
             
             mapView.DragButton = MouseButton.Left;
+
+            PointLatLng start = new PointLatLng(45.747774, 21.226252);
+            PointLatLng end = new PointLatLng(45.735784, 21.228215);
+
+            GMap.NET.MapProviders.GoogleMapProvider.Instance.ApiKey = "AIzaSyCMPm5rJtNTOivNQ8HdfKQDoUTClnDYH_w";
+            MapRoute route = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetRoute(start, end, false, false, 15);
            
+
+            if (route != null)
+            {
+                GMapRoute mRoute = new GMapRoute(route.Points);
+                {
+                    mRoute.ZIndex = -1;
+                }
+
+                mapView.Markers.Add(mRoute);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("There is no route");
+            }
+
         }
 
         private void aplicationExit(object sender, EventArgs e)
