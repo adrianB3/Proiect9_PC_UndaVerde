e7820cf126585d0f246d7d4836d4064c92b38579
adrianB3 <balanescuadrian71@gmail.com>
2018-04-25 11:42:30 +0300

    added dummy controls for testing

--- P9_UndaVerde/P9_UndaVerde/Properties/Resources.Designer.cs
+++ P9_UndaVerde/P9_UndaVerde/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace P9_UndaVerde.Properties {
+namespace TrafficSimTM.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace P9_UndaVerde.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("P9_UndaVerde.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TrafficSimTM.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
